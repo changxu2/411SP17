979e132148ebd3f5bad99ae13bafc0b47c228b3a
Songyu <lisongyu2009@gmail.com>
2017-03-12 16:10:11 -0500

    update db info

--- frontend/trip/functions.php
+++ frontend/trip/functions.php
@@ -1,7 +1,7 @@
 <?php
          function connectToDb() {
             $db = new mysqli('localhost', '
-tripubproject_adm', 'tripubproject_DB1', '12345shangshandalaohu');
+tripubproject_adm', '12345shangshandalaohu', 'tripubproject_DB1');
 
             if($db->connect_errno > 0) {
                 die('Unable to connect to database [' . $db->connect_error . ']');
