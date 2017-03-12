d7e1691e1426a476f2c271a427148e2883f105a8
Songyu <lisongyu2009@gmail.com>
2017-03-11 18:24:09 -0600

    finished codes

--- frontend/trip/functions.php
+++ frontend/trip/functions.php
@@ -11,13 +11,25 @@
          function createPlan($crr_user, $pre_user, $db) { //insert a new plan and return the id
             if (!$db->query("INSERT INTO Plan VALUES ($pre_user, $crr_user)")) {
                 echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
+                return NULL;
             }
             return $mysqli->insert_id;
+         }
+
+         function checkPlan($db, $pid) { //insert a new plan and return the id
+            if ($result = $db->query("SELECT Title, ArrayOfLocations FROM Plan WHERE planID = $pid")) {
+              $currentfield = mysqli_field_tell($result);
+              printf("Column %d:\n", $currentfield);
+              printf("Name:     %s\n", $finfo->name);
+              printf("Table:    %s\n", $finfo->table);
+              $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+              return $row;
+            }
+            echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
+            return NULL;
          }
 
          function closeDb($db) {
             $db->close();
          }
-
-
 ?>
