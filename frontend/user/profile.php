b102662f428826b2abf62732eb7bf8f2aa27841c
System Administrator <root@wirelessprv-10-192-170-135.near.illinois.edu>
2017-03-12 21:36:34 -0500

    profile done

--- frontend/user/profile.php
+++ frontend/user/profile.php
@@ -11,44 +11,48 @@ function createPlan($crr_user, $pre_user, $db) { //insert a new plan and return 
   return $db->insert_id;
  }
 
+  $userId = $_GET['user_id'];
+  $userName = $_SESSION['user_name'];
+  $userEmail = $_SESSION['user_email'];
+  $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+   if($db->connect_errno > 0) {
+      die('Unable to connect to database [' . $db->connect_error . ']');
+   }
+  $sql = "SELECT * FROM Plan WHERE createdByUserID = $userId";
+  $result = $db->query($sql) or die($db->error);
+  if (!$result) {
+      printf("Errormessage: %s\n", $db->error);
+  }
+
+  $ids = array();
+  $count = 0;
+
+  if ($result -> num_rows > 0) {
+    while ($row = $result -> fetch_assoc()) {
+      $ids[$count] = $row["planID"];
+      $count ++;
+    }
+  }
+  $len = $count;
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Tripub</title>
-      <link rel="stylesheet" href="css/style.css">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <link rel="stylesheet" href="css/signInUp.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <script src="js/signUp.js"></script>
 </head>
-<?php
-    $userId = $_GET['user_id'];
-    $userName = $_SESSION['user_name'];
-    $userEmail = $_SESSION['user_email'];
-    $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-     if($db->connect_errno > 0) {
-        die('Unable to connect to database [' . $db->connect_error . ']');
-     }
-    $sql = "SELECT * FROM Plan WHERE createdByUserID = $userId";
-    $result = $db->query($sql) or die($db->error);
-    if (!$result) {
-        printf("Errormessage: %s\n", $db->error);
-    }
-
-    $ids = array();
-    $count = 0;
-
-    if ($result -> num_rows > 0) {
-      while ($row = $result -> fetch_assoc()) {
-        $ids[$count] = $row["planID"]; 
-        $count ++;
-      }
-    }
-    $len = $count;
-?>
 
 <style>
 table {
@@ -104,29 +108,20 @@ button: hover {
 
 <body>
 
-    <ul class="nav nav-pills" style="background-color: aliceblue">
-        <a class="navbar-brand" href="#" style="padding-left: 1%">Triphub</a>
-        <li class="nav-item">
-            <a class="nav-link active" href="#">My Profile</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Create Trip Plan!</a>
-        </li>
-        <li class = "nav-item">
-            <a class = "nav-link" href = "login.php?logout" style ="padding-left: 670px">Logout</a >
-        </li>
-    </ul>
+  <ul class="nav nav-pills" style="background-color: aliceblue">
+      <a class="navbar-brand" href="#" style="padding-left: 1%">Triphub</a>
+      <li class="nav-item">
+          <a class="nav-link active" href="#">My Profile</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="#">Create Trip Plan!</a>
+      </li>
+      <li class = "nav-item">
+          <a class = "nav-link" href = "login.php?logout" style ="padding-left: 670px">Logout</a >
+      </li>
+  </ul>
 
-   <!--  <?
-    //while ($row = $result->fetch_assoc()) {
-      //  printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
-    }
-    ?> -->
-   <!--  <div> -->
 
-<!-- Hey, my dear <?//php echo $_SESSION['user_name']; ?>. You are logged in!!!!!!!
-You user id is: <?//echo $_GET['user_id']?>
-    </div> -->
 
 <h1> Profile Form </h1>
 
@@ -156,13 +151,17 @@ You user id is: <?//echo $_GET['user_id']?>
 <table id = "t01" style = "width: 100%">
   <?php for ($i = 0; $i < $count; $i++) { 
     //find the name of the plan with the ids array
-    $sql = "SELECT title FROM Plan WHERE planID = ids[$i]"; 
-    $title = $db->query($sql) or die($db->error);
-    if (!$titles) {
+    $to_find = $ids[$i];
+
+    $sql = "SELECT title FROM Plan WHERE planID = $to_find;"; 
+    $result_query = $db->query($sql) or die($db->error);
+    if (!$result_query) {
       printf("Errormessage: %s\n", $db->error);
-    } ?>
+    } 
+    $result_row = $result_query->fetch_object();
+    ?>
     <tr>
-      <td> $title </td>
+      <td><p><?php echo $result_row->title; ?></p></td>
     </tr>
   <?php } ?>
 </table>
