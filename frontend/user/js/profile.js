6ddc2967a39854ceedeeca0eb997c74c8517b497
Songyu <lisongyu2009@gmail.com>
2017-03-13 01:32:02 -0500

    finished

--- frontend/user/js/profile.js
+++ frontend/user/js/profile.js
@@ -1,5 +1,7 @@
-var jumpEdit = function(){
-  var url = window.location.hostname
-  url += ('/trip/createTrip.php?planID=' + this.id)
-  window.location(url);
-}
+$(document).ready(function() {
+  var jumpEdit = function(){
+    var url = window.location.hostname
+    url += ('/trip/createTrip.php?planID=' + this.id)
+    window.location(url);
+  }
+});
