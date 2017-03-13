6b2e54c06069f5293781cf2d4d2a769761887a90
Songyu <lisongyu2009@gmail.com>
2017-03-12 22:37:55 -0500

    lol

--- frontend/user/profile.php
+++ frontend/user/profile.php
@@ -173,29 +173,30 @@ button: hover {
    ?>
 </table>
 
+<div class = "container">
+  <ul class="list-group">
+    <?php
+    for ($i = 0; $i < $count; $i++) {
+      //find the name of the plan with the ids array
+      $to_find = $ids[$i];
 
-<ul class="list-group">
-  <?php
-  for ($i = 0; $i < $count; $i++) {
-    //find the name of the plan with the ids array
-    $to_find = $ids[$i];
-
-    $sql = "SELECT title FROM Plan WHERE planID = $to_find;";
-    $result_query = $db->query($sql) or die($db->error);
-    if (!$result_query) {
-      printf("Errormessage: %s\n", $db->error);
+      $sql = "SELECT title FROM Plan WHERE planID = $to_find;";
+      $result_query = $db->query($sql) or die($db->error);
+      if (!$result_query) {
+        printf("Errormessage: %s\n", $db->error);
+      }
+      $result_row = $result_query->fetch_object();
+      echo htmlspecialchars("<li class=\"list-group-item active\">".$result_row->title."</li>");
     }
-    $result_row = $result_query->fetch_object();
-    echo htmlspecialchars("<li class=\"list-group-item active\">".$result_row->title."</li>");
-  }
-  if (isset($_POST['addTrip'])){
-    if(!empty($_POST['addTrip'])){
-      $newPlanId = createPlan($userId, $userId, $db);
-      echo htmlspecialchars("<li class=\"list-group-item active\">New Plan</li>");
+    if (isset($_POST['addTrip'])){
+      if(!empty($_POST['addTrip'])){
+        $newPlanId = createPlan($userId, $userId, $db);
+        echo htmlspecialchars("<li class=\"list-group-item active\">New Plan</li>");
+      }
     }
-  }
-  ?>
-</ul>
+    ?>
+  </ul>
+</div>
 
 <div class = "container" style = "background-color:#f1f1f1">
   <form  method="post" action="./profile.php">
@@ -205,8 +206,6 @@ button: hover {
 </div>
 
   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
 </body>
 </html>
