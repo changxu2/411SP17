518b824cd0b2a8e4e15029816c4e8f42204c3c2f
Songyu <lisongyu2009@gmail.com>
2017-03-13 06:14:04 -0500

    324

--- frontend/trip/createTrip.php
+++ frontend/trip/createTrip.php
@@ -80,10 +80,10 @@ session_start();
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
             $latitude = $row['LATITUDE'];
             $longitude = $row['LONGITUDE'];
-            $sql = "SELECT ID, NAME, TYPE FROM locations WHERE (locations.Latitude < $latitude + 20) AND (locations.Latitude > $latitude - 20 ) AND (locations.Longitude < $longitude +20) AND (locations.Longitude > $longitude -20) AND (locations.NAME LIKE %place%)";
+            $sql = "SELECT ID, NAME, TYPE FROM locations WHERE (locations.Latitude < $latitude + 50) AND (locations.Latitude > $latitude - 50 ) AND (locations.Longitude < $longitude +20) AND (locations.Longitude > $longitude -20) AND (locations.NAME LIKE %$place%)";
             $result = $db->query($sql);
             $myArray = array();
-            while($row = $result->fetch_array()) {
+            while($row = $result->fetch_array(MYSQLI_ASSOC)) {
               $myArray[] = $row;
             }
             return $myArray;
