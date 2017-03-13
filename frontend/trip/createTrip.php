deab97f5d0efdea2a9e99822198221b1ca755d1d
Songyu <lisongyu2009@gmail.com>
2017-03-12 21:21:20 -0500

    fixed bug

--- frontend/trip/createTrip.php
+++ frontend/trip/createTrip.php
@@ -67,7 +67,7 @@
               <li class="list-group-item active">Plan: $res[0]['Title']</li>
               <?php
                     foreach ($res as $loc) {
-                      echo "<li class=\"list-group-item\">$loc['locations']['NAME'] $loc['locations']['TYPE']</li>";
+                      echo htmlspecialchars("<li class=\"list-group-item\">$loc['locations']['NAME'] $loc['locations']['TYPE']</li>");
                     }
               ?>
 
