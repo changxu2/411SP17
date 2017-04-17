b40acc50bd7e3540ceaef350ba243558b3e25580
Songyu <lisongyu2009@gmail.com>
2017-04-16 23:35:02 -0500

    test chat

--- chatroom/index.js
+++ chatroom/index.js
@@ -1,23 +1,25 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var sql = require('mssql')
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('room', function(room) {
+    socket.join(room);
+  })
+
+  console.log('a user connected')
   socket.on('disconnect', function(){
     console.log('a user has left')
   })
 
   socket.on('chat message', function(data){
     console.log(data)
-    io.emit('chat message', data)
+    io.to(data.room).emit('chat message', data.msg)
   })
-});
+})
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  console.log('listening on *:3000')
+})
